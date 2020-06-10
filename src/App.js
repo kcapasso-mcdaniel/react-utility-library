415aa3aae2cff9d1247fc5bc072285c77f3351c7
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-10 15:14:00 -0700

    add alphabetical order

--- src/App.js
+++ src/App.js
@@ -1,63 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/master.scss";
 import { uiData } from "./data/ui";
+import FunctionUI from "./components/FunctionUI";
+import orderBy from "lodash/orderBy";
 
-export default class App extends Component {
-   // react is giving us pre built classs we are giving it the name app it is extending their component class extending means adding stuff into this component - constructor as paged is being constructed render after data is rendered need to use the data inside of our app
+export default class App extends React.Component {
+   // dot notation to access the component - reach inside of react and pull out the component
+   // creating a class called App constructor executing
    constructor() {
       super();
       console.log(uiData);
    }
 
    render() {
-      const props = this.props;
-      const renderInputs = (num) => {
-         const inputs = [];
-         // for every number we have we are going to go through a loop and as long as less than zero increment
-         for (let i = 0; i < num; i++) {
-            // render some JSX
-            const id = `input-${props.name}-${i}`;
-            inputs.push(
-               <input
-                  type="text"
-                  className="form-control inline-action"
-                  key={id}
-                  id={id}
-               />
-            );
-         }
-         return inputs;
-      };
+      const orderedData = orderBy(uiData, "name", "asc");
 
+      // React will look out for this and needs to return JSK
+      // creating new variable and use orderBy method pass the collection, sort by what and what order returned
       return (
+         // wrap in parentheses when multiple html elements
          <div className="container">
             <div className="row">
-               {uiData.map((component) => {
+               {orderedData.map((functionUI) => {
                   return (
-                     // <FunctionComponent>
-                     <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-                        <p className="name">
-                           <b>{component.name}</b>&nbsp;-&nbsp;{component.desc}
-                        </p>
-                        <pre style={{ display: "none" }}>
-                           <code></code>
-                        </pre>
-                        <div className="actions float-right">
-                           {renderInputs(component.inputs)}
-                           <button className="btn btn-primary inline-action">
-                              Run
-                           </button>
-                        </div>
-                        <div className="clearfix mb-3"></div>
-                        <div
-                           className="alert alert-primary"
-                           style={{ display: "none" }}
-                        ></div>
-                        <div
-                           className="alert alert-danger"
-                           style={{ display: "none" }}
-                        ></div>
-                     </div>
+                     <FunctionUI
+                        name={functionUI.name}
+                        desc={functionUI.desc}
+                        inputs={functionUI.inputs}
+                     />
                   );
                })}
             </div>
---
+++ src/components/FunctionUI.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function FunctionUI(props) {
+   const renderInputs = (num) => {
+      const inputs = [];
+      // for every number we have we are going to go through a loop and as long as less than zero increment
+      for (let i = 0; i < num; i++) {
+         inputs.push(
+            <input type="text" className="form-control inline-action" />
+         );
+      }
+      return inputs;
+   };
+
+   return (
+      <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+         <p className="name">
+            <b>{props.name}</b>&nbsp;-&nbsp;{props.desc}
+         </p>
+         <pre style={{ display: "none" }}>
+            <code></code>
+         </pre>
+         <div className="actions float-right">
+            {renderInputs(props.inputs)}
+            <button className="btn btn-primary inline-action">Run</button>
+         </div>
+         <div className="clearfix mb-3"></div>
+         <div className="alert alert-primary" style={{ display: "none" }}></div>
+         <div className="alert alert-danger" style={{ display: "none" }}></div>
+      </div>
+   );
+}
